505fb56df17782453996425d928a0c941a545c0c
“AndreaFerreiro” <andreafga28@gmail.com>
2023-07-28 15:32:08 +0200

    add react-starter kit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+import {useLocation, matchPath} from 'react-router';
+import {Link, Route, Routes, useParams} from 'react-router-dom';
+function App() {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+    </div>
+  );
+}
+
+export default App;
