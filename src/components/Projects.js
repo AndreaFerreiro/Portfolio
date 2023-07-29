e543dcce4b773740884e83aaed4389d841431148
“AndreaFerreiro” <andreafga28@gmail.com>
2023-07-29 19:29:54 +0200

    add new projevts

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,8 @@ import '../styles/layout/projects.scss';
 import Anonymous from '../images/Anominous.png';
 import Disney from '../images/Disney.png';
 import RickMorty from '../images/RickMorty.png';
+import Awesome from '../images/Awesome.png';
+import Brujas from '../images/Brujas.png';
 import {Link} from 'react-router-dom';
 const Projects = () => {
     return (
@@ -9,12 +11,32 @@ const Projects = () => {
             <span className='title'>Bienvenid@ a mi Portfolio</span>
             <p className='desc'>Aquí podrás ver los proyectos que he ido desarrollando durante mi aprendizaje como programadora web.</p>
             <ul className="list">
+                <a target="_blank" className="linkhref" href="https://andreaferreiro.github.io/project-promo-t-module-1-team-4/">
+                    <li className="element">
+                    <img className="element__img" src={Brujas}></img>
+                    <div className="element__info">
+                        <span className="element__info--title">Brujas de la programación</span>
+                        <p className="element__info--desc">Página de presentación de las integrantes del grupo</p>
+                        <p className="element__info--tech">HTML5 - CSS3 - JS</p>
+                    </div>
+                    </li>
+                </a>
+                <a target="_blank" className="linkhref" href="http://beta.adalab.es/project-promo-t-module-2-team-3/">
+                    <li className="element">
+                    <img className="element__img" src={Awesome}></img>
+                    <div className="element__info">
+                        <span className="element__info--title">Awesome Profile Cards</span>
+                        <p className="element__info--desc">Creador de tarjetas virtuales personalizables</p>
+                        <p className="element__info--tech">HTML5 - CSS3 - JS</p>
+                    </div>
+                    </li>
+                </a>
                 <a target="_blank" className="linkhref" href="http://beta.adalab.es/modulo-3-evaluacion-final-AndreaFerreiro/#/">
                     <li className="element">
                     <img className="element__img" src={RickMorty}></img>
                     <div className="element__info">
                         <span className="element__info--title">Rick y Morty</span>
-                        <p className="element__info--desc">Proyecto de maquetación y programación web</p>
+                        <p className="element__info--desc">Visualizador y buscador de información sobre los personajes de la serie Rick Y Morty</p>
                         <p className="element__info--tech">HTML5 - CSS3 - JS</p>
                     </div>
                     </li>
@@ -34,7 +56,7 @@ const Projects = () => {
                     <img className="element__img" src={Disney}></img>
                     <div className="element__info">
                         <span className="element__info--title">Disney Characters</span>
-                        <p className="element__info--desc">Proyecto de maquetación y programación web</p>
+                        <p className="element__info--desc">Buscador de personajes Disney con posibilidad de añadir a "Favoritos"</p>
                         <p className="element__info--tech">HTML5 - CSS3 - JS</p>
                     </div>
                     </li>
