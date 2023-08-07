2a2cfc687904644ae4f668993d7ff993a805a455
“AndreaFerreiro” <andreafga28@gmail.com>
2023-08-07 12:17:05 +0200

    change projects links and order them

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,58 +18,60 @@ const Projects = () => {
                     <div className="element__info">
                         <span className="element__info--title">Programadoras Junior</span>
                         <p className="element__info--desc">Aplicación web en la que poder ver y/o subir proyectos</p>
-                        <p className="element__info--tech">HTML5 - CSS3 - REACT - EXPRESS - SQL - FREEDB</p>
+                        <p className="element__info--tech">HTML5 - CSS3 - REACT - EXPRESS - MySQL - FREEDB</p>
                     </div>
                     </li>
                 </a>
-                <a target="_blank" className="linkhref" href="https://andreaferreiro.github.io/project-promo-t-module-1-team-4/">
-                    <li className="element">
-                    <img className="element__img" src={Brujas}></img>
-                    <div className="element__info">
-                        <span className="element__info--title">Brujas de la programación</span>
-                        <p className="element__info--desc">Página de presentación de las integrantes del grupo</p>
-                        <p className="element__info--tech">HTML5 - CSS3 - JS</p>
-                    </div>
-                    </li>
-                </a>
+                
                 <a target="_blank" className="linkhref" href="http://beta.adalab.es/project-promo-t-module-2-team-3/">
                     <li className="element">
-                    <img className="element__img" src={Awesome}></img>
-                    <div className="element__info">
-                        <span className="element__info--title">Awesome Profile Cards</span>
-                        <p className="element__info--desc">Creador de tarjetas virtuales personalizables</p>
-                        <p className="element__info--tech">HTML5 - CSS3 - JS</p>
-                    </div>
+                        <img className="element__img" src={Awesome}></img>
+                        <div className="element__info">
+                            <span className="element__info--title">Awesome Profile Cards</span>
+                            <p className="element__info--desc">Creador de tarjetas virtuales personalizables</p>
+                            <p className="element__info--tech">HTML5 - CSS3 - JS</p>
+                        </div>
                     </li>
                 </a>
-                <a target="_blank" className="linkhref" href="http://beta.adalab.es/modulo-3-evaluacion-final-AndreaFerreiro/#/">
+                <a target="_blank" className="linkhref" href="https://andreaferreiro.github.io/modulo-3-evaluacion-final-AndreaFerreiro/">
                     <li className="element">
-                    <img className="element__img" src={RickMorty}></img>
-                    <div className="element__info">
-                        <span className="element__info--title">Rick y Morty</span>
-                        <p className="element__info--desc">Visualizador y buscador de información sobre los personajes de la serie Rick Y Morty</p>
-                        <p className="element__info--tech">HTML5 - CSS3 - JS</p>
-                    </div>
+                        <img className="element__img" src={RickMorty}></img>
+                        <div className="element__info">
+                            <span className="element__info--title">Rick y Morty</span>
+                            <p className="element__info--desc">Visualizador y buscador de información sobre los personajes de la serie Rick Y Morty</p>
+                            <p className="element__info--tech">HTML5 - CSS3 - JS</p>
+                        </div>
+                    </li>
+                </a>
+                <a target="_blank"  className="linkhref" href="https://andreaferreiro.github.io/modulo-2-evaluacion-final-AndreaFerreiro/">
+                    <li className="element">
+                        <img className="element__img" src={Disney}></img>
+                        <div className="element__info">
+                            <span className="element__info--title">Disney Characters</span>
+                            <p className="element__info--desc">Buscador de personajes Disney con posibilidad de añadir a "Favoritos"</p>
+                            <p className="element__info--tech">HTML5 - CSS3 - JS</p>
+                        </div>
                     </li>
                 </a>
                 <a target="_blank"  className="linkhref" href="http://beta.adalab.es/modulo-1-evaluacion-final-AndreaFerreiro/">
                     <li className="element">
-                    <img className="element__img" src={Anonymous}></img>
-                    <div className="element__info">
-                        <span className="element__info--title">Anonymous Proxy</span>
-                        <p className="element__info--desc">Proyecto de maquetación web</p>
-                        <p className="element__info--tech">HTML5 - CSS3</p>
-                    </div>
+                        <img className="element__img" src={Anonymous}></img>
+                        <div className="element__info">
+                            <span className="element__info--title">Anonymous Proxy</span>
+                            <p className="element__info--desc">Proyecto de maquetación web</p>
+                            <p className="element__info--tech">HTML5 - CSS3</p>
+                        </div>
                     </li>
                 </a>
-                <a target="_blank"  className="linkhref" href="http://beta.adalab.es/modulo-2-evaluacion-final-AndreaFerreiro/">
+                
+                <a target="_blank" className="linkhref" href="https://andreaferreiro.github.io/modulo-1-evaluacion-final-AndreaFerreiro/">
                     <li className="element">
-                    <img className="element__img" src={Disney}></img>
-                    <div className="element__info">
-                        <span className="element__info--title">Disney Characters</span>
-                        <p className="element__info--desc">Buscador de personajes Disney con posibilidad de añadir a "Favoritos"</p>
-                        <p className="element__info--tech">HTML5 - CSS3 - JS</p>
-                    </div>
+                        <img className="element__img" src={Brujas}></img>
+                        <div className="element__info">
+                            <span className="element__info--title">Brujas de la programación</span>
+                            <p className="element__info--desc">Página de presentación de las integrantes del grupo</p>
+                            <p className="element__info--tech">HTML5 - CSS3 - JS</p>
+                        </div>
                     </li>
                 </a>
             </ul>
